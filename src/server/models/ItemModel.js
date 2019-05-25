ee60b4d858b8d6c9bb0c05f096c362196f2490eb
can.yang <764271109@qq.com>
2019-05-26 00:11:50 +0800

    indent

--- src/server/models/ItemModel.js
+++ src/server/models/ItemModel.js
@@ -1,22 +1,22 @@
-export default function(sequelize, DataTypes) {
-    const Item = sequelize.define('Item', {
-        recordId: {
-            type: DataTypes.INTEGER,
-            field: 'record_id',
-            primaryKey: true
-        },
-        name: {
-            type: DataTypes.STRING,
-            field: 'name'
-        },
-        state: {
-            type: DataTypes.INTEGER,
-            field: 'state'
-        }
-    }, {
-        tableName: 'item',
-        timestamps: false
-    });
+export default function (sequelize, DataTypes) {
+  const Item = sequelize.define('Item', {
+    recordId: {
+      type: DataTypes.INTEGER,
+      field: 'record_id',
+      primaryKey: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      field: 'name'
+    },
+    state: {
+      type: DataTypes.INTEGER,
+      field: 'state'
+    }
+  }, {
+    tableName: 'item',
+    timestamps: false
+  });
 
-    return Item;
+  return Item;
 }
